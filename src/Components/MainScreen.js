575ed0239e52b8b835daee450df744786afdbb5e
Alexander Masztak <itzalqxander@gmail.com>
2023-03-12 01:05:44 -0600

    compatibility

--- src/Components/MainScreen.js
+++ src/Components/MainScreen.js
@@ -3,31 +3,29 @@ import { TypeAnimation } from 'react-type-animation';
 function MainScreen() {
   return (
     <div className="bg-[#08071F] h-screen">
-      <h1 className="text-2xl font-abc font-medium text-[#10D190] ml-48 mt-32 mb-8">Hi, my name is</h1>
-      <h1 className="text-6xl font-abc font-bold text-[#BDD4E7] ml-48 mb-8 ">Alexander Masztak</h1>
-      <h1 className="text-5xl font-abc font-bold text-[#919EB0] ml-48 mb-8">
-      <h1 className="text-5xl font-abc font-bold text-[#919EB0]">
-                        I create <span className=" font-bold">
-                            <TypeAnimation
-                                sequence={[
-                                    "Websites",
-                                    4000,
-                                    "Mobile Apps",
-                                    4000,
+      <h1 className="text-2xl font-abc font-medium text-[#10D190] ml-4 md:ml-48 mt-8 md:mt-32 mb-4 md:mb-8">Hi, my name is</h1>
+      <h1 className="text-4xl md:text-6xl font-abc font-bold text-[#BDD4E7] ml-4 md:ml-48 mb-4 md:mb-8 ">Alexander Masztak</h1>
+      <h1 className="text-3xl md:text-5xl font-abc font-bold text-[#919EB0] ml-4 md:ml-48 mb-4 md:mb-8">
+        I create <span className=" font-bold">
+          <TypeAnimation
+            sequence={[
+              "Websites",
+              4000,
+              "Mobile Apps",
+              4000,
 
-                                    () => {
-                                        console.log("Done typing!"); // Place optional callbacks anywhere in the array
-                                    },
-                                ]}
-                                wrapper="span"
-                                cursor={true}
-                                repeat={Infinity}
-                            />
-                        </span>
-                    </h1>
+              () => {
+                console.log("Done typing!"); // Place optional callbacks anywhere in the array
+              },
+            ]}
+            wrapper="span"
+            cursor={true}
+            repeat={Infinity}
+          />
+        </span>
       </h1>
-      
-      <p className="text-1xl font-abc text-white text-[#919EB0] ml-48">
+
+      <p className="text-base md:text-lg font-abc text-white text-[#919EB0] ml-4 md:ml-48">
         Currently, I am a student at{' '}
         <span className="text-[#10D190]"> Illinois State University</span>
         <br /> majoring in Computer Science - General. I focus on creating
@@ -36,5 +34,6 @@ function MainScreen() {
     </div>
   );
 }
+
 
 export default MainScreen;
