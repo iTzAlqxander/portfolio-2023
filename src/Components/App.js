9180de914165ac28be315760cc416ee75204d21d
Alexander Masztak <itzalqxander@gmail.com>
2023-03-13 01:58:24 -0500

    Projects page

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,42 +1,73 @@
 import { Link } from 'react-scroll';
+import React, { useState } from 'react';
 
 function App() {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const handleToggle = () => setShowLinks(!showLinks);
+
   return (
-    <div className="flex justify-end">
-      <Link
-        to="About"
-        smooth={true}
-        duration={500}
-        className="font-abc font-light hover:text-[#10D190] text-white font-bold py-2 px-4 rounded"
+    <nav className="flex items-center justify-end flex-wrap bg-[#08071F] p-6">
+      <div className="block lg:hidden ">
+        <button
+          className="flex  items-center px-3 py-2 border rounded text-white border-white hover:text-[#10D190] hover:border-[#10D190]"
+          onClick={handleToggle}
+        >
+          <svg
+            className="fill-current h-3 w-3 justify-end ml-auto"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div
+        className={`${
+          showLinks ? 'block' : 'hidden'
+        } w-full flex-grow lg:flex lg:items-center lg:w-auto`}
       >
-        <span className="text-[#10D190]">00.</span> About
-      </Link>
-      <Link
-        to="Projects"
-        smooth={true}
-        duration={500}
-        className="font-abc font-light hover:text-[#10D190] text-white font-bold py-2 px-4 rounded mx-2"
-      >
-        <span className="text-[#10D190]">01.</span> Projects
-      </Link>
-      <Link
-        to="Experience"
-        smooth={true}
-        duration={500}
-        className="font-abc font-light hover:text-[#10D190] text-white font-bold py-2 px-4 rounded"
-      >
-        <span className="text-[#10D190]">02.</span> Experience
-      </Link>
-      <Link
-        to="Contact"
-        smooth={true}
-        duration={500}
-        className="font-abc font-light hover:text-[#10D190] text-white font-bold py-2 px-4 rounded mx-2"
-      >
-        <span className="text-[#10D190]">03.</span> Contact
-      </Link>
-      
-    </div>
+        <div className="text-sm lg:flex-grow lg:text-right">
+          <Link
+            to="About"
+            smooth={true}
+            duration={500}
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-[#10D190] font-abc font-light mr-4"
+            onClick={() => setShowLinks(false)}
+          >
+            <span className="text-[#10D190]">00.</span> About
+          </Link>
+          <Link
+            to="Projects"
+            smooth={true}
+            duration={500}
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-[#10D190] font-abc font-light mr-4"
+            onClick={() => setShowLinks(false)}
+          >
+            <span className="text-[#10D190]">01.</span> Projects
+          </Link>
+          <Link
+            to="Experience"
+            smooth={true}
+            duration={500}
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-[#10D190] font-abc font-light mr-4"
+            onClick={() => setShowLinks(false)}
+          >
+            <span className="text-[#10D190]">02.</span> Experience
+          </Link>
+          <Link
+            to="Contact"
+            smooth={true}
+            duration={500}
+            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-[#10D190] font-abc font-light mr-4"
+            onClick={() => setShowLinks(false)}
+          >
+            <span className="text-[#10D190]">03.</span> Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
   );
 }
 
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,7 +1,27 @@
 function Projects() {
     return (
-      <div className="bg-green-500 h-screen">
-        <h1 id="Projects" className="text-3xl font-bold underline text-white">Projects</h1>
+      <div className="bg-black-700 h-screen lg:pl-48 lg:pr-48">
+
+      <h1 id="Projects" className="text-2xl font-abc font-medium text-[#10D190] mt-8 mb-4 md:mb-8">01. Projects</h1>
+      <div className="relative">
+      {/* Box with image */}
+      <div className="w-full md:w-3/4 md:h-auto bg-gray-200 rounded-lg overflow-hidden">
+        <img
+          className="w-full h-full object-cover object-center"
+          src="https://source.unsplash.com/random/800x600"
+          alt="Random"
+        />
+      </div>
+
+      {/* Overlapping rectangle for text */}
+      <div className="absolute top-1/3 right-0 transform md:w-1/3 md:h-1/3 bg-white rounded-lg p-8 shadow-lg">
+        <h2 className="text-2xl font-bold mb-4">Title of Text Box</h2>
+        <p className="text-gray-700">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+          gravida urna, id mattis lectus bibendum vitae. Nulla facilisi.
+        </p>
+      </div>
+    </div>
       </div>
     );
   }
